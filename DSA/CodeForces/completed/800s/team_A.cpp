067488bffccffe1acf88f519e6d509d4d88dc6ac
eduardosarno <eduartesarno@gmail.com>
2026-04-04 19:37:32 -0700

    bit++

--- DSA/CodeForces/completed/800s/team_A.cpp
+++ DSA/CodeForces/completed/800s/team_A.cpp
@@ -2,18 +2,9 @@
 
 /* Set up*/
 #include <cstdio>
-#include <fstream>
+
 #include <iostream>
 #include <ostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cmath>
-#include <climits>
 
 using namespace std;
 
