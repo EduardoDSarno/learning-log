937f700d4ec105469200ebe190c144490422cb7e
eduardosarno <eduartesarno@gmail.com>
2026-04-18 13:27:22 -0700

    ultra fast math

--- DSA/CodeForces/CPP/ultra_fast_math.cpp
+++ DSA/CodeForces/CPP/ultra_fast_math.cpp
@@ -3,18 +3,9 @@
 /* Set up*/
 #include <cstddef>
 #include <cstdio>
-#include <fstream>
 #include <iostream>
 #include <ostream>
-#include <vector>
-#include <algorithm>
 #include <string>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cmath>
-#include <climits>
 
 using namespace std;
 using ll = long long;
